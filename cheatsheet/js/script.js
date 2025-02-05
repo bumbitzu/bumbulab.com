d1c56a70690dae4172b495911e87776180608b00
bumbitzu <bumbitzu@users.noreply.github.com>
2025-02-06 01:01:05 +0200

    update

--- cheatsheet/js/script.js
+++ cheatsheet/js/script.js
@@ -4,14 +4,16 @@ function navigate(url)
 }
 function searchCheatSheets()
 {
-    var input, filter, categories,butttons;
+    var input, filter, categories,buttons;
     input = document.getElementById('search');
     filter = input.value.toUpperCase();
     categories = document.getElementById('categories');
-    buttons = categories.getElementsByTagName('button');
+    buttons = categories.getElementsByTagName('a');
 
-    for (var i = 0; i < buttons.length; i++) {
-        if (buttons[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+    for (var i = 0; i < buttons.length; i++) 
+    {
+        let span = buttons[i].getElementsByTagName('span')[0];
+        if (span.innerHTML.toUpperCase().indexOf(filter) > -1) {
             buttons[i].style.display = "";
         } else {
             buttons[i].style.display = "none";
